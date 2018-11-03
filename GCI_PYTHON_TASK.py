042cc4c90ef4f20e121c8c83eaeda6a2ac95c434
hung04 <44702872+hung04@users.noreply.github.com>
2018-11-03 23:22:05 +0700

    Update GCI_PYTHON_TASK.py

--- GCI_PYTHON_TASK.py
+++ GCI_PYTHON_TASK.py
@@ -1,5 +1,5 @@
 for x in range (0,10):
     print("GCI is great")
-a = input ("What is your name ? ")
-print ("Hello " + a + ", please to meet you!")
-print ("Did you know that your name backwards is", a[::-1])
+name = input ("What is your name ? ")
+print ("Hello " + name + ", please to meet you!")
+print ("Did you know that your name backwards is", name[::-1])
