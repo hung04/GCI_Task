53c9f79ecc95f10d85b80f6994ea29846e9ef097
hung04 <44702872+hung04@users.noreply.github.com>
2018-11-03 23:21:06 +0700

    Add files via upload

---
+++ GCI_PYTHON_TASK.py
@@ -0,0 +1,5 @@
+for x in range (0,10):
+    print("GCI is great")
+a = input ("What is your name ? ")
+print ("Hello " + a + ", please to meet you!")
+print ("Did you know that your name backwards is", a[::-1])
